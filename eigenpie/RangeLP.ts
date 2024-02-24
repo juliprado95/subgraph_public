8e1c2bcf64c56fe2f222d5c18a564dc891ba5cdc
juliprado95 <161124148+juliprado95@users.noreply.github.com>
2024-02-24 07:31:43 -0300

    Update RangeLP.ts
    simplified version

--- eigenpie/RangeLP.ts
+++ eigenpie/RangeLP.ts
@@ -3,17 +3,24 @@ import { Transfer as TransferEvent, LiquidityAdded as LiquidityAddedEvent, Liqui
 import { mlrtLiquidityStatus } from "../generated/schema"
 import { BIGINT_ZERO, ETHER_ONE } from "./constants"
 
+function isSwapAddress(address: Address): boolean {
+    return (
+        address.equals(Address.fromHexString("0x350d6d813be7b64681f91f16a98ef360bd42b66b")) || // wstETH/mstETH
+        address.equals(Address.fromHexString("0x6177811663a60ac211566be5873c5ed441d9e948"))    // swETH/mswETH
+    );
+}
+
 function updateLiquidity(contractAddress: Address, blockTimestamp: BigInt): void {
     let RangeLp = RangeLP.bind(contractAddress)
-    let status = mlrtLiquidityStatus.load("RangeLP-".concat(contractAddress.toHexString()))
+    let status = mlrtLiquidityStatus.load("RangeLP-" + contractAddress.toHexString())
 
     if (!status) {
-        status = new mlrtLiquidityStatus("RangeLP-".concat(contractAddress.toHexString()))
+        status = new mlrtLiquidityStatus("RangeLP-" + contractAddress.toHexString())
     }
 
-    let storedBalabce = RangeLp.getUnderlyingBalances()
-    status.token0Balance = storedBalabce.getAmount0Current()
-    status.token1Balance = storedBalabce.getAmount1Current()
+    let storedBalance = RangeLp.getUnderlyingBalances()
+    status.token0Balance = storedBalance.getAmount0Current()
+    status.token1Balance = storedBalance.getAmount1Current()
     status.totalSupply = RangeLp.totalSupply()
     status.mlrtPerShare = status.token0Balance.times(ETHER_ONE).div(status.totalSupply)
     status.updateTimestamp = blockTimestamp
@@ -21,16 +28,7 @@ function updateLiquidity(contractAddress: Address, blockTimestamp: BigInt): void
 }
 
 export function handleTransfer(event: TransferEvent): void {
-    // swap transacrtion
-    if ( // wstETH/mstETH
-        event.params.from == Address.fromHexString("0x350d6d813be7b64681f91f16a98ef360bd42b66b") ||
-        event.params.to == Address.fromHexString("0x350d6d813be7b64681f91f16a98ef360bd42b66b")
-    ) {
-        updateLiquidity(event.address, event.block.timestamp)
-    } else if ( // swETH/mswETH
-        event.params.from == Address.fromHexString("0x6177811663a60ac211566be5873c5ed441d9e948") ||
-        event.params.to == Address.fromHexString("0x6177811663a60ac211566be5873c5ed441d9e948")
-    ) {
+    if (isSwapAddress(event.params.from) || isSwapAddress(event.params.to)) {
         updateLiquidity(event.address, event.block.timestamp)
     }
 }
